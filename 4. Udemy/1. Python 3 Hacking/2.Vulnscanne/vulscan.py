88f8027df53efa1f73ee679d07576e6541277edf
albert18 <erertech@gmail.com>
2022-12-16 19:45:45 +0300

    update

--- 4. Udemy/1. Python 3 Hacking/2.Vulnscanne/vulscan.py
+++ 4. Udemy/1. Python 3 Hacking/2.Vulnscanne/vulscan.py
@@ -5,4 +5,15 @@ port_number = int(input('[+] * Enter Amount Of Ports You Want To Scan(500 - firs
 vuln_file = input('[+] * Enter Path To The File With Vulnerable Softwares: ')
 print('\n')
 
-target = portscanner.PortScan()
+target = portscanner.PortScan(targets_ip, port_number)
+target.scan()
+
+with open(vuln_file, 'r') as file:
+    count = 0
+    for banner in target.banners:
+        file.seek(0)
+        for line in file.readlines():
+            if line.strip() in banner:
+                print('[!!] VULNERABLE BANNER: "' + banner + '" ON PORT: ' + str(target.open_ports[count]))
+        count += 1
+            
