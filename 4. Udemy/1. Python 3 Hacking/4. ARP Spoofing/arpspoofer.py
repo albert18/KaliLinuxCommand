fccfd62005a338f16719a293595e289802b75a49
albert18 <erertech@gmail.com>
2022-12-19 19:37:26 +0300

    update

--- 4. Udemy/1. Python 3 Hacking/4. ARP Spoofing/arpspoofer.py
+++ 4. Udemy/1. Python 3 Hacking/4. ARP Spoofing/arpspoofer.py
@@ -1,23 +1,3 @@
-# import scapy.all as scapy
-# import sys
-# import time
-
-# def get_mac_address(ip_address):
-#     broadcast_layer = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
-#     arp_layer = scapy.ARP(pdst=ip_address)
-#     get_mac_packet = broadcast_layer/arp_layer
-#     answer = scapy.srp(get_mac_packet, timeout=2, verbose=False)[0]
-#     return answer[0][1].hwsrc
-
-# target_ip = str(sys.argv[2])
-# router_ip = str(sys.argv[1])
-# target_mac = str(get_mac_address(target_ip))
-# router_mac = str(get_mac_address(router_ip))
-
-# print(router_mac)
-# print(target_mac)
-
-
 import scapy.all as scapy
 import sys
 import time
@@ -47,8 +27,3 @@ try:
 except KeyboardInterrupt:
     print('Closing ARP Spoofer.')
     exit(0)
-
-
-
-
-
