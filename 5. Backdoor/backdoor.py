e1a93e881d77dc2146e61b79c1a6a1ec66a66fd3
albert18 <erertech@gmail.com>
2022-12-22 19:56:01 +0300

    update

--- 5. Backdoor/backdoor.py
+++ 5. Backdoor/backdoor.py
@@ -3,6 +3,6 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1'), 5555)
 message = 'Hello World'
-s.send(message)
+s.send(message.encode())
 
 
--- 5. Backdoor/server.py
+++ 5. Backdoor/server.py
@@ -3,7 +3,8 @@ import termcolor
 
 def target_communication():
     message = target.recv(1024)
-    print(message)
+    print(message.decode())
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 5555))
